9173ffdb59ad0368f44691159086e4955d4079e4
Dave Paunesku <daveponet@github.com>
2015-11-24 12:04:17 -0800

    updated order because column operations need value replacement

--- util.R
+++ util.R
@@ -5,11 +5,12 @@ source("install_gymnast_packages.R")
 source("util_function_groups/extended_base.R")
 source("util_function_groups/string_cleaning.R")
 source("util_function_groups/datatypes.R")
+source("util_function_groups/value_replacement.R")
 source("util_function_groups/messages.R")
 source("util_function_groups/printing_tables.R")
 source("util_function_groups/math.R")
 source("util_function_groups/column_operations.R")
 source("util_function_groups/reshaping.R")
-source("util_function_groups/value_replacement.R")
+
 
 
