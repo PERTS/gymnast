d3b987a396d80eb383c462de414d8917f3f5593c
Chris Macrander <cmacrander@gmail.com>
2016-10-17 11:33:47 -0700

    installation at top so our functions mask others
    not vice versa

--- R/util.R
+++ R/util.R
@@ -9,6 +9,36 @@
 
 ###############################################################
 ###
+###     Package Installation
+###
+###############################################################
+
+gymnast_install <- function () {
+    # Load (or install) all packages used by gymnast.
+    dependencies <- c(
+        "digest","dplyr","formatR","ggplot2",
+        "grid","Hmisc","knitr","lme4",
+        "lmerTest","psych","reshape2","scales" ,
+        "stargazer","stringr","xtable"
+    )
+    for(lib_name in dependencies){
+        message(paste0("Loading... ",lib_name))
+        # Require returns FALSE if packages failed
+        if( !require(lib_name, character.only = TRUE) ){
+            install.packages(lib_name)
+            # library raises error if installation failed
+            library(lib_name, character.only = TRUE)
+        }
+    }
+}
+
+# Run if you source the code directly (e.g. from github), but NOT run if
+# included as a package. In the latter case, manually call gymnast_install().
+gymnast_install()
+
+
+###############################################################
+###
 ###     Extended Base
 ###     Improves basic functionality of R.
 ###
@@ -661,32 +691,3 @@ util.find_crypt_paths <- function (files_to_load, initial_path = NA,
     return(found_paths)
 }
 
-
-###############################################################
-###
-###     Package Installation
-###
-###############################################################
-
-gymnast_install <- function () {
-    # Load (or install) all packages used by gymnast.
-    dependencies <- c(
-        "digest","dplyr","formatR","ggplot2",
-        "grid","Hmisc","knitr","lme4",
-        "lmerTest","psych","reshape2","scales" ,
-        "stargazer","stringr","xtable"
-    )
-    for(lib_name in dependencies){
-        message(paste0("Loading... ",lib_name))
-        # Require returns FALSE if packages failed
-        if( !require(lib_name, character.only = TRUE) ){
-            install.packages(lib_name)
-            # library raises error if installation failed
-            library(lib_name, character.only = TRUE)
-        }
-    }
-}
-
-# Run if you source the code directly (e.g. from github), but NOT run if
-# included as a package. In the latter case, manually call gymnast_install().
-gymnast_install()
