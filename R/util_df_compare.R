28c6ef436212a69c72de265ca1d5ca74bad6b771
Dan <dkgreene@stanford.edu>
2017-12-21 21:03:07 -0800

    first commit - starting util_df_compare module

---
+++ R/util_df_compare.R
@@ -0,0 +1,16 @@
+##########################################################################
+###
+###     util_df_compare.R
+###     Dan Greene, 12/21/17
+###
+###     The goal of the util_df_compare module is to make it easy to see
+###     where two data frames differ, for debugging and further analysis.
+###
+###     Depends on util.R
+###
+
+
+# REMOVE SECTION BEFORE FINALIZING
+setwd("~/Sites/gymnast/R")
+source("util.R")
+
