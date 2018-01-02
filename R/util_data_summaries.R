0a0ed7003a6c18ccc525055c8180337c01cddef7
Dan <dkgreene@stanford.edu>
2018-01-02 09:19:13 -0800

    removing testing code from util_data_summaries

--- R/util_data_summaries.R
+++ R/util_data_summaries.R
@@ -315,12 +315,6 @@ ds.build_glm1_formulas <- function(
 
 }
 
-for(formula in formulas) {
-  print(formula)
-  str_split(formula,"[ ]*~[ ]*")[[1]] %>% print()
-}
-
-
 ds.helper$glm1_formula_to_varlist <- function(formula) {
     # input is a canonical ds.glm1 formula,
     # see ds.validate_glm1_model
