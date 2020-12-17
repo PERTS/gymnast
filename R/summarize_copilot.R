15cfad6ce4e18fc7a6806a3066431c3887e98cda
Chris Macrander <chris@perts.net>
2020-12-17 11:27:39 -0800

    drops summarize_copilot$merge_participants
    @sarah-gripshover-PERTS post-hoc review please;
    
    This won't break anything in rserve until we pull in the changes over there. I also checked that this is not used in analysis.

--- R/summarize_copilot.R
+++ R/summarize_copilot.R
@@ -29,39 +29,6 @@ util <- import_module("util")
 
 `%+%` <- paste0
 
-merge_participants <- function(triton.participant, neptune.participant) {
-  # Args are rows directly from db tables, with prefixed column names.
-  # names(triton.participant) %in% c('participant.uid', 'participant.in_target_group', ...)
-  # names(neptune.participant) %in% c('participant.uid', 'participant.name', ...)
-  #
-  # Returns df with names 'participant_id' and 'in_target_group' where the
-  # latter is %in% c(0, 1, NA).
-
-  if (!all(triton.participant$in_target_group %in% c(0, 1))) {
-    stop("Triton participant target group values not all in (0, 1).")
-  }
-
-  # Rename some triton columns for clarity. Things get pretty confusing since
-  # both tables are named "participant".
-  triton_for_join <- triton.participant %>%
-    select(
-      triton.stripped_student_id = participant.stripped_student_id,
-      triton.team_id = participant.team_id,
-      in_target_group = participant.in_target_group
-    )
-
-  neptune.participant %>%
-    left_join(
-      triton_for_join,
-      by = c(
-        participant.organization_id = "triton.team_id",
-        participant.name = "triton.stripped_student_id"
-      )
-    ) %>%
-    select(participant.uid, in_target_group) %>%
-    rename(participant_id = participant.uid)
-}
-
 team_class <- function(triton.program,
                        triton.team,
                        triton.classroom,
