5d8af37ee6327b5234217ed8602556c553a4b405
sarahgripshover <sarah.grip@gmail.com>
2021-01-12 15:57:11 -0600

    quick fixes to get clean_qualtrics to work right

--- R/util_qualtrics_cleaning.R
+++ R/util_qualtrics_cleaning.R
@@ -12,7 +12,6 @@
 library("dplyr")
 util_legacy <- import_module("util_legacy")
 
-
 STANDARD_SECOND_ROW_QUALTRICS_COLUMNS <- c(
   "ResponseID"
   ,"ResponseSet"
@@ -236,14 +235,14 @@ qc.clean_qualtrics <- function(
   # takes a Qualtrics data.frame, a boolean indicating whether unnamed columns
   # should be removed, a function (qc.extract_delimited, by default), and
   # optional arguments to be passed to a user-created function fun
-  qdf_char <- util_legacy$util..to_character(qdf)
-  qdf_unicode <- util_legacy$util..to_ascii(qdf_char)
+  qdf_char <- util_legacy$util.to_character(qdf)
+  qdf_unicode <- util_legacy$util.to_ascii(qdf_char)
   qdf_rn <- qc.rename_columns(qdf_unicode,
     remove_unnamed_columns,
     extract_column_name=extract_column_name,
     ...)
   qdf_row_removed <- qdf_rn[-1,]
-  qdf_clean <- util_legacy$util..as_numeric_if_number(qdf_row_removed)
+  qdf_clean <- util_legacy$util.as_numeric_if_number(qdf_row_removed)
   return(qdf_clean)
 }
 
